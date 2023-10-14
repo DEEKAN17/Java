4be275741890b798658d4209ddc8307dbafe3e20
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-10-14 22:37:33 +0530

    Create Find a day.java

---
+++ Youtube/switch Statement/Find a day.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                           Switch Statement 
+                               H.w
+              finding a day with user input
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int a;
+	    Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number between 1-7:");
+		a = scanner.nextInt();
+		
+		switch(a)
+		{
+		case 1:
+		    System.out.println("Monday");
+		    break;
+		case 2:
+		    System.out.println("Tuesday");
+		    break;
+		case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
+            break;
+		}
+		
+	}
+}
